0caba9a928073fd48a59255e24ba6dbce911c0cd
Christopher Sims <7741705+ChristopherSims@users.noreply.github.com>
2021-03-04 20:49:10 -0500

    hotfix

--- input.py
+++ input.py
@@ -2,10 +2,10 @@
 ncore = 50 # number of cores for calculation
 ECUT = 60 # Energy cutoff in Ryberg
 DFT = "ESPRESSO" #ESPRESSO, SIESTA, ABINIT, VASP
-APIKEY = "4XK0y0eziIorGoHP0M" #APIKEY for mat
+APIKEY = "YourAPIKEY" #APIKEY for mat
 MATERIALID = "mp-20630" # input for pymatgen
 SEEDNAME = "HMS" #your own name or default
-EMAIL = "christopherssims95@gmail.com"
+EMAIL = "yyy@mail.com"
 SOC = True # TRUE or FALSE
 KMESH = [4,4,4] # KMESH for nscf/wan
 NUMBANDS = 120 # Number of bands
